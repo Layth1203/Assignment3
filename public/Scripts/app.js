806fc86f28721b65ed28dc52030dbf0bbf823bfb
Layth1203 <layth.quick@ontariotechu.net>
2023-11-02 22:56:08 -0400

    A2Update

---
+++ public/Scripts/app.js
@@ -0,0 +1,8 @@
+// IFFI --> Immediately invoked functio expression 
+(function(){
+    function Start()
+    {
+        console.log("App Started");
+    }
+    window.addEventListener("load", Start);
+})();
